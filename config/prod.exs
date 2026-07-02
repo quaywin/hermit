d5230f7c42b76c9d13e51aa322e85ad387614ae7
Thang Nguyen <quaywin@gmail.com>
2026-07-02 18:27:09 +0700

    Disable SSL enforcement in production

--- config/prod.exs
+++ config/prod.exs
@@ -10,14 +10,14 @@ config :hermit, HermitWeb.Endpoint, cache_static_manifest: "priv/static/cache_ma
 # Force using SSL in production. This also sets the "strict-security-transport" header,
 # known as HSTS. If you have a health check endpoint, you may want to exclude it below.
 # Note `:force_ssl` is required to be set at compile-time.
-config :hermit, HermitWeb.Endpoint,
-  force_ssl: [
-    rewrite_on: [:x_forwarded_proto],
-    exclude: [
-      # paths: ["/health"],
-      hosts: ["localhost", "127.0.0.1"]
-    ]
-  ]
+# config :hermit, HermitWeb.Endpoint,
+#   force_ssl: [
+#     rewrite_on: [:x_forwarded_proto],
+#     exclude: [
+#       # paths: ["/health"],
+#       hosts: ["localhost", "127.0.0.1"]
+#     ]
+#   ]
 
 # Configure Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Req

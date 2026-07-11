ad3ec4044bd2b3a2a7821ee89993e10ba61c1b7a
Thang Nguyen <quaywin@gmail.com>
2026-07-11 13:43:40 +0700

    fix: configure endpoint check_origin using PHX_HOST

--- config/runtime.exs
+++ config/runtime.exs
@@ -69,7 +69,11 @@ if config_env() == :prod do
 
   config :hermit, HermitWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
-    check_origin: :conn,
+    check_origin: [
+      "https://#{host}",
+      "http://#{host}",
+      "//#{host}"
+    ],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
